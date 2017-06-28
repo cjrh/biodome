285660c25ecc8bb402fb64f73d95e159901026f6
Caleb Hattingh <caleb.hattingh@gmail.com>
2017-06-28 22:35:56 +1000

    First commit.

---
+++ biodome.py
@@ -0,0 +1,30 @@
+import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def biodome(name, default=None, cast=None):
+    # type: (str, Any, Callable) -> Any
+    raw_value = os.environ.get(name).strip()
+    print(raw_value)
+    if raw_value is None:
+        return default
+
+    # Use the same type as default as the cast
+    type_ = cast or type(default)
+
+    if bool in (cast, type_):
+        return raw_value.lower() in (
+            '1', 'y', 'yes', 'on', 'active', 'activated', 'enabled', 'true',
+            't', 'ok', 'yeah',
+        )
+
+    try:
+        if type_ in (dict, list, set, tuple):
+            raw_value = eval(raw_value)
+            return (type(raw_value) == type_ and raw_value) or default
+        return type_(raw_value)
+    except:
+        return default
---
+++ tests/test_biodome.py
@@ -0,0 +1,121 @@
+import os
+import pytest
+from biodome import biodome
+
+
+@pytest.mark.parametrize('name,default,setting,result', [
+    ('X', 2, '123', 123),
+    ('X', '2', '123', '123'),
+    ('X', True, '123', False),
+    ('X', True, '', False),
+    ('X', True, '0', False),
+    ('X', True, 't', True),
+    ('X', True, '1', True),
+    ('X', True, 'y', True),
+    ('X', True, 'yes', True),
+    ('X', True, 'YES', True),
+    ('X', True, 'Yes', True),
+    ('X', True, 'Y', True),
+    ('X', True, 'True', True),
+    ('X', True, 'true', True),
+    ('X', True, 'TRUE', True),
+    ('X', True, 'on', True),
+    ('X', True, 'On', True),
+    ('X', True, 'ON', True),
+    ('X', True, 'active', True),
+    ('X', True, 'ACTIVE', True),
+    ('X', True, 'Active', True),
+    ('X', False, 'y', True),
+    ('X', False, 'yes', True),
+    ('X', False, 'YES', True),
+    ('X', False, 'Yes', True),
+    ('X', False, 'Y', True),
+    ('X', False, 'True', True),
+    ('X', False, 'true', True),
+    ('X', False, 'TRUE', True),
+    ('X', False, 'on', True),
+    ('X', False, 'On', True),
+    ('X', False, 'ON', True),
+    ('X', False, 'active', True),
+    ('X', False, 'ACTIVE', True),
+    ('X', False, 'Active', True),
+
+    ('X', False, 'yas', False),
+    ('X', False, 'blah', False),
+    ('X', False, '123', False),
+    ('X', False, '____', False),
+    ('X', False, 'heyy', False),
+
+    ('X', 123, 'heyy', 123),
+    ('X', -123, '-123', -123),
+    ('X', 0, '0', 0),
+
+    ('X', 0.1, '0', 0),
+    ('X', 0.1, '1.2', 1.2),
+    ('X', -0.1, '-1.2', -1.2),
+    ('X', 1, '1.0', 1),
+
+    ('X', 'heya', 'blah', 'blah'),
+
+    ('X', {}, '{"a": 1}', dict(a=1)),
+    ('X', {}, '{"a": "blah", "b": "bleh"}', dict(a='blah', b='bleh')),
+
+    ('X', [], '[1, 2, 3]', [1, 2, 3]),
+    ('X', [], '[]', []),
+    ('X', [], '[', []),
+    ('X', [], '[blah]', []),
+    ('X', [], '["blah"]', ['blah']),
+
+    ('X', {1, 2, 3}, '{1, 2}', {1, 2}),
+    ('X', {1, 2, 3}, '{"1":2}', {1, 2, 3}),
+
+    ('X', (1, 2), 'blah', (1, 2)),
+    ('X', (), 'blah', ()),
+    ('X', (), '(1, 2)', (1, 2)),
+    ('X', (), '(1,2)', (1, 2)),
+    ('X', (), '(   1   ,   2   )', (1, 2)),
+    ('X', (), '(1)', ()),
+    ('X', (), '(1,)', (1,)),
+
+    ('X', '0', '1.053', '1.053'),
+
+])
+def test_param(name, default, setting, result):
+    os.environ[name] = str(setting)
+    assert biodome(name, default) == result
+
+
+@pytest.mark.parametrize('name,cast,setting,result', [
+    ('X', bool, 'blah', False),
+    ('X', bool, '1', True),
+    ('X', bool, '0', False),
+    ('X', bool, 'True', True),
+    ('X', bool, 'False', False),
+    ('X', bool, 'true', True),
+    ('X', bool, 'false', False),
+
+    ('X', float, 'blah', None),
+    ('X', float, '1', 1.0),
+    ('X', float, '1', 1),
+    ('X', float, '1e-17', 1e-17),
+
+    ('X', int, 'blah', None),
+    ('X', int, '1', 1),
+    ('X', int, '1.0', None),
+
+    ('X', str, '1.0', '1.0'),
+    ('X', str, '1', '1'),
+
+    ('X', list, '1', None),
+    ('X', list, '[1]', [1]),
+
+    ('X', set, '{2}', {2}),
+    ('X', set, 'set([1, 2, 3])', {1, 2, 3}),
+
+    ('X', dict, '{"a": 123}', dict(a=123)),
+    ('X', dict, 'dict(a=123)', dict(a=123)),
+
+])
+def test_cast(name, cast, setting, result):
+    os.environ[name] = str(setting)
+    assert biodome(name, cast=cast) == result
